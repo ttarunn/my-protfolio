15cbaa871f0dec4b60539bf37c5fbbdf68856a2f
Tarun <ttarunn0709@gmail.com>
2023-09-24 21:51:40 +0530

    fix width

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -57,7 +57,7 @@ const ComputerCanvas = ()=> {
     <Canvas frameloop="demand"
     shadows
     camera={{ position: [20, 3, 5], fov: 25 }}
-    gl={{preserveDrawingBuffer: true}} className="lg:ml-28">
+    gl={{preserveDrawingBuffer: true}}>
       <Suspense fallback={<CanvasLoader/>}>
         <OrbitControls
           enableZoom={false}
